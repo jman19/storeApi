d6e14e506dde58bd02f73747735aec0f75745e3a
jason <jasonguyw@gmail.com>
2019-07-06 20:50:52 -0400

    created more test data in sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,12 @@
 INSERT INTO product (title, price, inventory_Count) VALUES
 ('Shoes',100.00,100),
 ('Laser',100000.00,0),
-('IPhone',500.00,30),
+('IPhone',500.00,0),
 ('Peanut Butter',15.00,400),
 ('Gum',0.50,100),
 ('T-Shirt',20.25,50),
 ('Watch',200.00,20),
-('YoYo',60.00,0);
+('YoYo',60.00,0),
+('Computer',1000.00,1),
+('Pens',10.00,100),
+('Monitor',200.23,10);
